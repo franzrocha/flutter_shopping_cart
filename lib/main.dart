10ba4fa7ff13b727b884bde2fb1697afb90348d0
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-07-28 22:20:32 +0800

    final shopping cart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_cart/data_class.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+    create: (context)=>DataClass(),
+    child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage()
+    ),);
+  }
+}
